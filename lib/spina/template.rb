f6de96b086d9d4b2eb2781093c992f786e30ed47
Bram Jetten <mail@bramjetten.nl>
2013-08-31 11:54:35 +0200

    Initial commit

---
+++ lib/spina/template.rb
@@ -0,0 +1,7 @@
+require "spina/template/version"
+
+module Spina
+  module Template
+    # Your code goes here...
+  end
+end
---
+++ lib/spina/template/version.rb
@@ -0,0 +1,5 @@
+module Spina
+  module Template
+    VERSION = "0.0.1"
+  end
+end
