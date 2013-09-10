ad6d54b6b73ce8c0f19de6d602b878779e2a2a50
Bram Jetten <mail@bramjetten.nl>
2013-09-10 16:23:41 +0200

    Template

--- lib/spina/template.rb
+++ lib/spina/template.rb
@@ -3,6 +3,7 @@ require "spina/template/version"
 module Spina
   module Template
     class Engine < ::Rails::Engine
+      Rails.application.config.assets.precompile += %w(wysihtml5.css)
     end
   end
 end
