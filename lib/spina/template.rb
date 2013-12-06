1e279d03c0a1c8e81314e5c0c044205d5d74197e
Bram Jetten <mail@bramjetten.nl>
2013-12-06 20:25:37 +0100

    Bump version naar 0.1.1

--- lib/spina/template.rb
+++ lib/spina/template.rb
@@ -1,9 +1,12 @@
 require "spina/template/version"
+require 'bourbon'
+require 'neat'
+require 'jquery-fileupload-rails'
 
 module Spina
   module Template
     class Engine < ::Rails::Engine
-      config.assets.precompile += %w(wysihtml5.css)
+      # config.assets.precompile += %w(wysihtml5.css)
     end
   end
 end
